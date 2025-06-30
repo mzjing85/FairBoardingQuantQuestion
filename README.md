e420963d74c58bd86e3d60b6dad7761825808ae4
mzjing85 <jing.m@wustl.edu>
2025-06-29 23:54:31 -0500

    two example models done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FairBoardingQuantQuestion
+
+Link to Youtube Video: https://www.youtube.com/watch?v=NXs6hijbqkI&list=PLADLdjl79_mhdmhdxA1Gk8sklGEWJkEKq&index=2
+
---
+++ RandomSelectionModel.py
@@ -0,0 +1,33 @@
+#Modeling the First Come First Serve (FCFS) scheduling algorithm
+
+import random
+
+points = [] #create an empty list to store the points
+maxPoolSize = 100 #set the maximum pool size for passengers
+numIterations = 1000 #set the number of iterations for the simulation
+numSeats = 50 #set the number of seats available (should be less than maxPoolSize)
+
+for i in range(1, maxPoolSize + 1):
+
+    cumulativeProb = [0 for _ in range(i)] # initialize cumulative probabilities for each passenger to 0
+
+    for j in range(1, numIterations+1):
+        # for i passengers, we are running j iterations of the simulation
+
+        sample_size = min(50, i)
+        selected_indices = random.sample(range(i), sample_size)
+
+        for passenger in range(i):
+            if passenger in selected_indices:
+                cumulativeProb[passenger] = ((cumulativeProb[passenger] * (j - 1) + 1)) / j
+            else:
+                cumulativeProb[passenger] = (cumulativeProb[passenger] * (j - 1)) / j
+
+
+    for x in range(i):
+        points.append([i, cumulativeProb[x]])
+
+#printing 
+print("Starting Simulation: \n")
+for point in points:
+    print(f"Num Passengers: {point[0]}, Cumulative Probability: {point[1]:.4f}")
